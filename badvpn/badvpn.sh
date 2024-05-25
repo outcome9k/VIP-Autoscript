551e9d7455898021a0badde0159803a06b966f95
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-05-25 11:57:40 +0100

    Update badvpn.sh

--- badvpn/badvpn.sh
+++ badvpn/badvpn.sh
@@ -1,9 +1,9 @@
 wget -O /bin/badvpn-udpgw https://github.com/FasterExE/VIP-Autoscript/raw/main/badvpn/badvpn-udpgw
-wget -O /etc/systemd/system/badvpn.service https://github.com/FasterExE/VIP-Autoscript/raw/main/badvpn/badvpn-7100-7900.service
+wget -O /etc/systemd/system/badvpn-7100-7900.service https://github.com/FasterExE/VIP-Autoscript/raw/main/badvpn/badvpn-7100-7900.service
 chmod 777 /bin/badvpn-udpgw
-chmod 777 /etc/systemd/system/badvpn.service
+chmod 777 /etc/systemd/system/badvpn-7100-7900.service
 
 sudo systemctl daemon-reload
-sudo systemctl enable badvpn.service
-sudo systemctl start badvpn.service
-sudo systemctl restart badvpn.service
+sudo systemctl enable badvpn-7100-7900.service
+sudo systemctl start badvpn-7100-7900.service
+sudo systemctl restart badvpn-7100-7900.service
