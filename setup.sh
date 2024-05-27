5bf2c399be50136ee182742b390b8e031fa6d08e
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-05-27 10:54:15 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -118,6 +118,7 @@ echo -e "${BIGreen}--->${NC}  ${BIYellow}★ ${NC}${BICyan} Install TOOLS${NC}${
 sleep 2
 wget -q https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/tools.sh;chmod +x tools.sh;./tools.sh
 rm tools.sh
+sudo timedatectl set-timezone GMT
 mkdir /etc/ilyass/telegram
 wget -O /etc/ilyass/telegram/key.sh https://raw.githubusercontent.com/FasterExE/KEY-BOT/main/key.sh; bash /etc/ilyass/telegram/key.sh
 clear
