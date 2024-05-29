90befa890792c85235cf30ed29fc72d8394bf83b
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-05-29 20:32:05 +0100

    Update ovpn.sh

--- ssh/ovpn.sh
+++ ssh/ovpn.sh
@@ -201,6 +201,11 @@ iptables-restore -t < /etc/iptables.up.rules
 netfilter-persistent save
 netfilter-persistent reload
 
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -s 10.6.0.0/24 -o eth0 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s 10.7.0.0/24 -o eth0 -j MASQUERADE
+sudo iptables-save > /etc/iptables/rules.v4
+
 systemctl enable openvpn
 systemctl start openvpn
 /etc/init.d/openvpn restart
