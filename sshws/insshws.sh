b43fd8da9a64ed511ba61f4d98cd09ba1a2e889c
𓆩 𝐈𝐋𝐘𝐀𝐒𝐒 𓆪 <139225454+FasterExE@users.noreply.github.com>
2024-06-04 09:58:10 +0100

    Add files via upload

---
+++ sshws/insshws.sh
@@ -0,0 +1,39 @@
+cd
+apt install python -y
+apt install python3 -y
+
+wget -O /usr/local/bin/WebSocket https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/sshws/WebSocket.py
+wget -O /usr/local/bin/WebSocket.SSH https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/sshws/WebSocket.SSH.py
+wget -O /usr/local/bin/WebSocket.OVPN https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/sshws/WebSocket.OVPN.py
+
+chmod +x /usr/local/bin/WebSocket
+chmod +x /usr/local/bin/WebSocket.SSH
+chmod +x /usr/local/bin/WebSocket.OVPN
+
+
+wget -O /etc/systemd/system/WebSocket.service https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/sshws/WebSocket.service && chmod +x /etc/systemd/system/WebSocket.service
+
+wget -O /etc/systemd/system/WebSocket.SSH.service https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/sshws/WebSocket.SSH.service && chmod +x /etc/systemd/system/WebSocket.SSH.service
+
+wget -O /etc/systemd/system/WebSocket.OVPN.service https://raw.githubusercontent.com/FasterExE/VIP-Autoscript/main/sshws/WebSocket.OVPN.service && chmod +x /etc/systemd/system/WebSocket.OVPN.service
+
+
+systemctl daemon-reload
+
+systemctl enable WebSocket.service
+systemctl start WebSocket.SSH.service
+systemctl restart WebSocket.OVPN.service
+
+systemctl enable WebSocket.service
+systemctl start WebSocket.SSH.service
+systemctl restart WebSocket.OVPN.service
+
+systemctl enable WebSocket.service
+systemctl start WebSocket.SSH.service
+systemctl restart WebSocket.OVPN.service
+
+mkdir /etc/ilyass/ws > /dev/null 2>&1
+rm /etc/ilyass/ws/status
+rm /etc/ilyass/ws/status2
+echo 'SSH ' >> /etc/ilyass/ws/status
+echo 'SSH' >> /etc/ilyass/ws/status2
